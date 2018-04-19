0053cdcad25e550217f71705d90b976fe9cdf02d
tramplpf <3013470059@qq.com>
2018-04-19 09:01:09 +0800

    添加了一个linux下初始化脚本

--- linux/init/init.sh
+++ linux/init/init.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 
+# 安装 epel 扩展仓库 
+yum install epel-release -y 
+
+
 # install the w3m
 yum install w3m w3m-img -y;
 
@@ -10,3 +14,28 @@ yum install telnet -y;
 
 # install zip and unzip
 yum install zip unzip -y;
+
+
+mkdir /opt/package
+
+chmod 777 /opt/package
+
+
+## wget -O /opt/package/myget-0.1.2.tar.gz http://myget.sourceforge.net/release/myget-0.1.2.tar.gz
+## tar -xzvf /opt/package/myget-0.1.2.tar.gz  -C /opt/package/
+# cd /opt/package/myget-0.1.2
+# ./configure
+#make && meka install
+
+#mytget -help
+
+
+# install rar package  
+##wget -O /opt/package/ https://www.rarlab.com/rar/rarlinux-x64-5.5.0.tar.gz
+##mkdir /usr/local/rar
+##tar -xzvf /opt/package/rarlinux-x64-5.5.0.tar.gz  -C /usr/local/rar
+##ln -s /usr/local/rar/rar/rar /usr/local/bin/rar 
+##ln -s /usr/local/rar/rar/unrar /usr/local/bin/unrar 
+
+
+# display the time and user of history 
