031834ed0c7d47650b40dd2c2e93a41ba5e8db5f
tramplpf <pengfei_lpf@126.com>
2018-04-20 19:23:29 +0800

    添加安装iftop命令的sh文件

--- linux/init/install_iftop.sh
+++ linux/init/install_iftop.sh
@@ -6,6 +6,7 @@ yum install flex byacc  libpcap ncurses ncurses-devel libpcap-devel
 # downlaod the source code
 wget -P /opt/package/  http://www.ex-parrot.com/pdw/iftop/download/iftop-0.17.tar.gz
 
+mkdir -p /usr/local/software
 tar xzvf /opt/package/iftop-0.17.tar.gz -C /usr/local/software/
 
 #ln -s /usr/local/software/iftop-0.17 /usr/local/software/iftop
