7ab1ff84449dd06f8e625339ac3829726653bc18
tramplpf <pengfei_lpf@126.com>
2018-04-21 22:01:23 +0800

    修改了init.sh

--- linux/init/init.sh
+++ linux/init/init.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+# install wget 
+yum install wget -y
 
-## mv /etc/yum.repos.d/CentOS-Base.repo CentOS-Base.repo.bak  
-## wget -O /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/epel-7.repo  
-## yum clean all  
-## yum  makecache  
+# 下面的这个地址好像不对了。里面的配置不全 
+ mv /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo.bak  
+ wget -O /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/epel-7.repo  
+ yum clean all  
+ yum  makecache  
 
 # 安装 epel 扩展仓库 
 yum install epel-release -y 
@@ -26,8 +29,8 @@ mkdir /opt/package
 chmod 777 /opt/package
 
 
-## wget -O /opt/package/myget-0.1.2.tar.gz http://myget.sourceforge.net/release/myget-0.1.2.tar.gz
-## tar -xzvf /opt/package/myget-0.1.2.tar.gz  -C /opt/package/
+# wget -O /opt/package/myget-0.1.2.tar.gz http://myget.sourceforge.net/release/myget-0.1.2.tar.gz
+# tar -xzvf /opt/package/myget-0.1.2.tar.gz  -C /opt/package/
 # cd /opt/package/myget-0.1.2
 # ./configure
 #make && meka install
@@ -36,11 +39,11 @@ chmod 777 /opt/package
 
 
 # install rar package  
-##wget -O /opt/package/ https://www.rarlab.com/rar/rarlinux-x64-5.5.0.tar.gz
-##mkdir /usr/local/rar
-##tar -xzvf /opt/package/rarlinux-x64-5.5.0.tar.gz  -C /usr/local/rar
-##ln -s /usr/local/rar/rar/rar /usr/local/bin/rar 
-##ln -s /usr/local/rar/rar/unrar /usr/local/bin/unrar 
+wget -O /opt/package/ https://www.rarlab.com/rar/rarlinux-x64-5.5.0.tar.gz
+mkdir /usr/local/rar
+tar -xzvf /opt/package/rarlinux-x64-5.5.0.tar.gz  -C /usr/local/rar
+ln -s /usr/local/rar/rar/rar /usr/local/bin/rar 
+ln -s /usr/local/rar/rar/unrar /usr/local/bin/unrar 
 
 
 # display the time and user of history 
