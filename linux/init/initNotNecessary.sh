c1e92114e36c0658a13f3f8e87c2da1b0adcd676
tramplpf <3013470059@qq.com>
2018-12-11 22:18:54 +0800

    添加一个软件包

--- linux/init/initNotNecessary.sh
+++ linux/init/initNotNecessary.sh
@@ -7,3 +7,11 @@ yum install NetworkManager-tui  -y
 #远程使用图形界面的时候需要
 yum install xhost  -y 
 
+
+# xclock 命令用于测试图形界面是否可以正确映射 。 检查LINUX 下 DISPLAY 变量设置是否正确。 
+yum install xclock  -y 
+
+
+# 使用xdpinfo 可以查看当前显示的详细的信息
+yum install xdpinfo -y 
+
